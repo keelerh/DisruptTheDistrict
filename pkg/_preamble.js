bf393fab3cd25288c52e15308ea3e1fe949798f0
Keeley Erhardt <kerhardt@circle.com>
2017-02-11 20:01:01 -0500

    Add all the stuffs

---
+++ pkg/_preamble.js
@@ -0,0 +1,14 @@
+
+/*
+** Annotator v1.2.10-dev-abc8c9e
+** https://github.com/okfn/annotator/
+**
+** Copyright 2017, the Annotator project contributors.
+** Dual licensed under the MIT and GPLv3 licenses.
+** https://github.com/okfn/annotator/blob/master/LICENSE
+**
+** Built at: 2017-02-12 00:12:30Z
+ */
+
+
+//# sourceMappingURL=_preamble.map
